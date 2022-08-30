1d20704f34bfd54c0cbefc840b3b0b6d8b331dc3
fivetran-arminefendic <armin.efendic@fivetran.com>
2022-08-29 21:10:47 -0600

    Refactor code + comment clean up

--- gsheet_data.py
+++ gsheet_data.py
@@ -1,68 +1,68 @@
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
 import re
 import requests
+# Disable warning for HTTPS requests being made without certificate verifiication
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
+disable_warnings(InsecureRequestWarning)
 
 def main(request):
-    # the type of service we need access too (spreadsheets with readonly)
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    # --- Authentication and Authorization for service acct to google sheet --- #
+    scopes = ['https://www.googleapis.com/auth/spreadsheets']
+    service_account_file = 'service_acct_key.json'
+    creds = service_account.Credentials.from_service_account_file(service_account_file, scopes=scopes)
 
+    # --- Google sheet variables --- #
+    spreadsheet_id = '1wv9tncWB4yF8PxxEcyvHXFHxdEDqMUiTbUCYA8A4KXE'
+    range = 'codes'
 
-    SERVICE_ACCOUNT_FILE = '/Users/armin.efendic/Documents/strava_api/service_acct_key.json'
-
-
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-    SPREADSHEET_ID = '1wv9tncWB4yF8PxxEcyvHXFHxdEDqMUiTbUCYA8A4KXE'
-    RANGE = 'codes'
-
+    # --- Grab google sheet values --- #
     try:
         service = build('sheets', 'v4', credentials=creds)
-
-        # Call the Sheets API
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                    range=RANGE).execute()
+        result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                    range=range).execute()
         values = result.get('values')
-        # Remove header
-        values.pop(0)
+        values.pop(0) # Remove header of google sheet
 
-        # refresh_tokens = get_refresh_tokens()
     except HttpError as err:
         print(err)
 
+    # --- Get refresh tokens and update google sheet --- #
+    refresh_tokens = update_gsheet(request, values, sheet, spreadsheet_id, range)
 
-    row_num = 1 # set to 1 to offset for the header row
+    # --- Prepare the data for post processing - remove duplicates and null values --- #
+    refresh_tokens = flatten_list(refresh_tokens)
+    refresh_tokens = [*set((filter(None, refresh_tokens)))]
+
+    return(refresh_tokens)
+
+def update_gsheet(request, values, sheet, spreadsheet_id, range):
+    row_num = 1 # offset for the header row in the sheet
     row_numbers = []
-    # google needs a list of lists for the values
     new_refresh_tokens = []
     refresh_tokens = []
-    # grad the cells that do not have a refresh token
+
+    # --- Check each "row" (value) if it contains a refresh token i.e. ['url', 'refresh_token'] --- #
     for value in values:
-        row_num += 1 # "iterates" thru the rows of the sheet
+        row_num += 1
         if len(value) == 2:
             refresh_tokens.append(value[1])
 
         if len(value) == 1:
-            row_numbers.append(row_num) # mark the row number
-            refresh_token = get_refresh_token(request,value[0]) # pass in the url
+            row_numbers.append(row_num)
+            refresh_token = get_refresh_token(request,value[0])
             new_refresh_tokens.append(refresh_token)
             refresh_tokens.append(refresh_token)
 
+    # --- Update the google sheet --- #
     if len(new_refresh_tokens) > 0:
-        post = sheet.values().append(spreadsheetId=SPREADSHEET_ID, range='codes!B'+str(row_numbers[0]), 
-                                        valueInputOption='RAW', body={'values':new_refresh_tokens}).execute() # values requires to be a list of lists
-    
-    refresh_tokens = flatten_list(refresh_tokens)
+        post = sheet.values().append(spreadsheetId=spreadsheet_id, range=f'{range}!B'+str(row_numbers[0]), 
+                                        valueInputOption='RAW', body={'values':new_refresh_tokens}).execute()
 
-    # Filter out all None values and remove duplicates for post processing purposes
-    # None values will get added to the if the post request doesn't return a new refresh token
-    refresh_tokens = [*set((filter(None, refresh_tokens)))]
-    # print(refresh_tokens)
-    return(refresh_tokens)
+    return refresh_tokens
 
 def get_refresh_token(request,url):
     auth_url = 'https://www.strava.com/oauth/token'
@@ -84,29 +84,10 @@ def get_refresh_token(request,url):
 
 def flatten_list(_2d_list):
     flat_list = []
-    # Iterate through the outer list
     for element in _2d_list:
         if type(element) is list:
-            # If the element is of type list, iterate through the sublist
             for item in element:
                 flat_list.append(item)
         else:
             flat_list.append(element)
     return flat_list
-
-# --- For Testing --- #
-# if __name__ == '__main__':
-#     main(
-#         {
-#             'state': {
-#                 '5dc2bef4b13f135c5ddab9563e74742eec1fa3df': 1658880473
-#             },
-#             'secrets': {
-#                 'client_id': '91382',
-#                 'client_secret': '20a49adb714cc2314bbdf5f1514a6b86cd4838eb',
-#                 'is_pag': {
-#                     '5dc2bef4b13f135c5ddab9563e74742eec1fa3df': 2
-#                 }
-#             },
-#             'hasMore': True
-#         })
